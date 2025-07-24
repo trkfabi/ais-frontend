c6f5a1bd8d78b21652b536fb627453279fef6793
Fabian Martinez <fmartinez@inzori.com>
2025-07-24 13:07:54 +0200

    - Send device id so we can store device's bounding boxes

--- api/Api.ts
+++ api/Api.ts
@@ -1,8 +1,11 @@
 import { Vessel, Vessels, Bounds } from "../config/types";
 
 import { fixBoundsOrder, getUTCMinutesAgo } from "../utils/Conversion";
+import Constants from "expo-constants";
+import * as Device from "expo-device";
+
 const BASE_URL =
-  process.env.EXPO_PUBLIC_API_URL || "http://192.168.1.14:3000/api/";
+  process.env.EXPO_PUBLIC_API_URL || "http://localhost:3000/api/";
 
 export const fetchVessels = async (
   actualBounds: Bounds = null,
@@ -14,8 +17,17 @@ export const fetchVessels = async (
   if (actualBounds) {
     apiUrl += `&bounds=${JSON.stringify(correctedBounds)}`;
   }
-  //console.log(apiUrl);
-  const response = await fetch(apiUrl);
+  // Get a unique device identifier
+  // const deviceId =
+  //   Constants?.installationId || Constants?.deviceId || "unknown-device";
+
+  const deviceId = Device.osInternalBuildId || Device.deviceName || "unknown";
+  const response = await fetch(apiUrl, {
+    headers: {
+      "x-device-id": deviceId,
+    },
+  });
+  console.log("Fetching vessels from:", apiUrl);
 
   if (!response.ok) {
     throw new Error("Failed to fetch vessels");
